c48a24a33379cbb4391bc93b14ddbe519464bdf1
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-11-08 18:00:46 +0100

    Added livefeed shadow

--- src/containers/Landing/components/LiveFeed.js
+++ src/containers/Landing/components/LiveFeed.js
@@ -26,6 +26,7 @@ const styles: Object = {
         height: 'auto',
         backgroundColor: 'white',
         overflowX: 'hidden',
+        boxShadow: '0px 1px 4px 0px rgba(0,0,0,0.1)',
     },
     wrapper: {
         padding: 16,
